ea9f47af8a095b1173363746ac4ad15ee8ea9598
David Heidrich <me@bowlingx.com>
2025-04-21 18:44:41 +0200

    chore: renamed package back

--- test-setup.js
+++ test-setup.js
@@ -1,5 +1,6 @@
 import "@testing-library/jest-dom/extend-expect";
 import "regenerator-runtime/runtime";
+import "intersection-observer"
 
 //Fix for "matchMedia not present, legacy browsers require a polyfill jest" error
 window.matchMedia =
